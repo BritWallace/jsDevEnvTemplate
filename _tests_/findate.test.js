99d42a5e554607f837ebf1283818607da3c048da
Brit Wallace <britwallace888@gmail.com>
2021-12-20 19:05:15 -0800

    first commit

---
+++ _tests_/findate.test.js
@@ -0,0 +1,23 @@
+import findate from '../src/findate.js';
+
+describe("findate", () => {
+  test("should return the day of the week with any inputted date.", () => {
+    const getDate = new Date("December 25, 2021"); 
+    expect(findate = .Saturday);
+  });
+});
+
+expect(findate(getDate)).toEqual("Saturday");
+
+
+
+
+
+
+
+
+
+
+
+
+// const dayArray  [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
